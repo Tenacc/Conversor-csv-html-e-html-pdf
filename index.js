d81735d9b6dee0603225662d42da7cbc9ba8c756
Thiago <thiago@pop-os.localdomain>
2022-07-08 16:00:43 -0300

    promisefy function para retornar dados

--- index.js
+++ index.js
@@ -2,4 +2,11 @@ var Reader = require('./Reader')
 
 var leitor = new Reader()
 
-leitor.Read('./planilha alvo.csv')
+
+
+async function main() {
+    var dados = await leitor.Read('./planilha alvo.csv')
+    console.log(dados)
+}
+
+main()
